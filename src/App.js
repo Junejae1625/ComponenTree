947f2fc8463039d606eaeb26cf19295bfb8b000c
Jaejune <quswowns345@naver.com>
2023-01-12 18:51:00 +0900

    [2023/01/12] Feature: errorPage

--- src/App.js
+++ src/App.js
@@ -13,11 +13,28 @@ function App() {
       <RecoilRoot>
         <LayoutPage>
           <Routes>
-            <Route path="/loading" element={<LoadingPage />}></Route>
             <Route path="/" element={<RendingPage />}></Route>
+            <Route path="/loading" element={<LoadingPage />}></Route>
             <Route path="/graph1" element={<ForceDirectedGraphPage />}></Route>
             <Route path="/graph2" element={<PieGraphPage />}></Route>
             <Route path="/graph3" element={<BarGraphPage />}></Route>
+            <Route
+              path="*"
+              element={
+                <div
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    fontSize: "3rem",
+                  }}
+                >
+                  404 Not FoundPage
+                </div>
+              }
+            ></Route>
           </Routes>
         </LayoutPage>
       </RecoilRoot>
